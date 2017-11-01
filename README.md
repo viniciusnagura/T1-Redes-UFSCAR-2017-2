619191ff37fcf7d70e0e2ff99e40185ce034288e
viniciusnagura <viniciusnagura@hotmail.com>
2017-11-01 11:23:03 -0700

    Versao final do identificador dos comandos passados pelo HTML, e adicao no README das instrucoes para configuracao do Apache2

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # T1-Redes-UFSCAR-2017-2
+
 Trabalho 1 da disciplina de Redes de Computadores, do 2º Semestre de 2017, da Universidade Federal de São Carlos.
 
-Descrição [aqui](https://github.com/viniciusnagura/T1-Redes-UFSCAR-2017-2/blob/master/doc/Descri%C3%A7%C3%A3o%20do%20Projeto.pdf)
+#### Descrição do projeto
+[Clique aqui](https://github.com/viniciusnagura/T1-Redes-UFSCAR-2017-2/blob/master/doc/Descri%C3%A7%C3%A3o%20do%20Projeto.pdf).
+
+#### Instruções para rodar o projeto:
+
+* Editar o arquivo apache2.conf em /etc/apache2/apache2.conf
+	```
+	sudo vi /etc/apache2/apache2.conf
+	```
+* Em seguida, substituir:
+	```
+	<Directory /var/www/>
+	     Options Indexes FollowSymLinks
+	     AllowOverride None
+	     Require all granted
+	</Directory>
+	```
+	pelo path da pasta src do projeto. Por exemplo:
+	```
+	<Directory /home/aluno/Desktop/T1/T1-Redes-UFSCAR-2017-2/src/>
+		Options Indexes FollowSymLinks
+		AllowOverride None
+		Require all granted
+	</Directory>
+	```
+* Depois, adicionar
+	```
+	ScriptAlias "cgi-bin" "***path_da_pasta_cgi-bin_do_projeto***"
+	```
+	e substituir
+	```
+	<Directory /usr/lib/cgi-bin/>
+		Options ExecCGI
+		SetHandler cgi-script
+	</Directory>
+	```
+	também pelo path da pasta cgi-bin do projeto. Por exemplo:
+	```
+	 ScriptAlias "/cgi-bin/" "/home/aluno/Desktop/T1/T1-Redes-UFSCAR-2017-2/src/cgi-bin/"
+	 <Directory /home/aluno/Desktop/T1/T1-Redes-UFSCAR-2017-2/src/cgi-bin/>
+	 		Options ExecCGI
+	 		SetHandler cgi-script
+	 </Directory>
+	 ```
+	* Por fim, reiniciar o Apache2
+	 ```
+	 sudo service apache2 restart
---
+++ src/cgi-bin/webserver.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import cgi
+import cgitb
+import os
+
+cgitb.enable()
+
+# -------------------------------------------
+#      F U N C O E S   A U X I L I A R E S   |
+# -------------------------------------------
+
+def le_parametros_html():
+
+	# Efetua a leitura dos parametros passados pelo HTML, retornando uma lista de comandos
+	# no seguinte formato: (nro_maquina, nro_comando, argumento_comando)
+	#	nro_comando:
+	#		1 - ps
+	#		2 - df
+	#		3 - finger
+	#		4 - uptime
+
+	# Leitura dos parametros do HTML 
+	parametros_html = cgi.FieldStorage()
+
+	# Tuplas de comandos passados pelo HTML
+	tuplas_html = []
+	for parametro in parametros_html:
+		if(parametro[0:3] == 'maq'):
+			if(parametro[4] == '_'):
+				tuplas_html.append((parametro[3],parametro[5:len(parametro)], ''))
+			else:
+				tuplas_html.append((parametro[3],parametro[5:len(parametro)], parametros_html[parametro].value))
+
+	# Validacao das tuplas de comandos (duplicata de comandos e consistencia de checkbox)
+	tuplas_validadas = []
+	for comando in ('ps', 'df', 'finger', 'uptime'):
+		for i in ('1','2','3'):
+			tupla_sem_argumento = None
+			tupla_com_argumento = None
+			for tupla in tuplas_html:
+				if(tupla[0] == i and tupla[1] == comando):
+					if(tupla[2] == ''):
+						tupla_sem_argumento = tupla
+					else:
+						tupla_com_argumento = tupla
+			if tupla_sem_argumento is not None:
+				if tupla_com_argumento is not None:
+					tuplas_validadas.append(tupla_com_argumento)
+				else:
+					tuplas_validadas.append(tupla_sem_argumento)
+
+	# Conversao dos comandos ps, df, finger e uptime nos numeros 1, 2, 3 e 4, respectivamente
+	tuplas_finais = []
+	for tupla in tuplas_validadas:
+		if(tupla[1] == 'ps'):
+			tuplas_finais.append((int(tupla[0]), 1, tupla[2]))
+		elif (tupla[1] == 'df'):
+			tuplas_finais.append((int(tupla[0]), 2, tupla[2]))
+		elif (tupla[1] == 'finger'):
+			tuplas_finais.append((int(tupla[0]), 3, tupla[2]))
+		elif (tupla[1] == 'uptime'):
+			tuplas_finais.append((int(tupla[0]), 4, tupla[2]))
+
+	return tuplas_finais
+
+
+# -------------------------------------------
+#                  M A I N                   |
+# -------------------------------------------
+
+print "Content-type: text/html\r\n\r\n";
+print "<font size=+1>Environment</font></br>";
+
+print "<pre>"
+
+# Recebe os comandos a serem passados para as maquinas
+lista_comandos = le_parametros_html()
+
+# Mostra os comandos identificados na tela
+for x in lista_comandos:
+	print x
+	print "<br>"
+
+print "</pre>"
+
